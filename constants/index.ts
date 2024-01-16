b48dc267943320f15b67fe1c200ec675a9a55cf3
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-16 16:49:36 +0600

    Add default values for headerLinks, note, grade, event, and task

---
+++ constants/index.ts
@@ -0,0 +1,47 @@
+export const headerLinks = [
+  {
+    label: "Dashboard",
+    route: "/",
+  },
+  {
+    label: "Tasks",
+    route: "/tasks",
+  },
+  {
+    label: "Notes",
+    route: "/notes",
+  },
+  {
+    label: "Grades",
+    route: "/grades",
+  },
+  {
+    label: "Schedule",
+    route: "/schedule",
+  },
+];
+
+export const noteDefaultValues = {
+  title: "",
+  content: "",
+  categoryId: "",
+};
+
+export const gradeDefaultValues = {
+  assignment: "",
+  credits: 0,
+  letter: "",
+};
+
+export const eventDefaultValues = {
+  title: "",
+  description: "",
+  dateTime: new Date(),
+};
+
+export const taskDefaultValues = {
+  title: "",
+  description: "",
+  deadline: new Date(),
+  priorityId: "",
+};
