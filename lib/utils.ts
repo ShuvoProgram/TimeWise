6795038c0adb04875727f244ba2b8928d36e1014
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-16 13:12:06 +0600

    Add utility functions and date formatting

---
+++ lib/utils.ts
@@ -0,0 +1,144 @@
+import { type ClassValue, clsx } from "clsx";
+
+import { twMerge } from "tailwind-merge";
+import qs from "query-string";
+
+import { UrlQueryParams, RemoveUrlQueryParams } from "@/types";
+import { auth } from "@clerk/nextjs";
+import { getAllGradesByUser } from "./database/actions/grade.actions";
+import { getLetterById } from "./database/actions/letter.actions";
+import { Grades } from "@/components/gradesTable/columns";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
+
+export function removeKeysFromQuery({
+  params,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
+
+export const handleError = (error: unknown) => {
+  console.error(error);
+  throw new Error(typeof error === "string" ? error : JSON.stringify(error));
+};
+
+export function formatDateString(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString(undefined, options);
+
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${formattedDate} at ${time}`;
+}
+
+export const multiFormatDateString = (timestamp: string = ""): string => {
+  const timestampNum = Math.round(new Date(timestamp).getTime() / 1000);
+  const date: Date = new Date(timestampNum * 1000);
+  const now: Date = new Date();
+
+  const diff: number = now.getTime() - date.getTime();
+  const diffInSeconds: number = diff / 1000;
+  const diffInMinutes: number = diffInSeconds / 60;
+  const diffInHours: number = diffInMinutes / 60;
+  const diffInDays: number = diffInHours / 24;
+
+  switch (true) {
+    case Math.floor(diffInDays) >= 30:
+      return formatDateString(timestamp);
+    case Math.floor(diffInDays) === 1:
+      return `${Math.floor(diffInDays)} day ago`;
+    case Math.floor(diffInDays) > 1 && diffInDays < 30:
+      return `${Math.floor(diffInDays)} days ago`;
+    case Math.floor(diffInHours) >= 1:
+      return `${Math.floor(diffInHours)} hrs ago`;
+    case Math.floor(diffInMinutes) >= 1:
+      return `${Math.floor(diffInMinutes)} mins ago`;
+    default:
+      return "Just now";
+  }
+};
+
+export function formatSimpleDate(inputDate: string): string {
+  const date = new Date(inputDate);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear());
+
+  return `${day}/${month}/${year}`;
+}
+
+export async function getData(): Promise<Grades[]> {
+  const { sessionClaims } = auth();
+  const creator = sessionClaims?.userId as any;
+
+  const getGrades = await getAllGradesByUser({ creator: creator });
+  const grades = getGrades?.data;
+
+  // Map the grades to the desired format
+  const mappedGrades: Grades[] = await Promise.all(
+    grades.map(async (grade: any) => {
+      const letter = await getLetterById(grade.letter);
+
+      return {
+        id: grade._id,
+        assignment: grade.assignment || "N/A",
+        credits: grade.credits || "N/A",
+        created: formatSimpleDate(grade.createdAt) || "N/A",
+        grade: letter ? (letter.grade as string) : "N/A",
+      };
+    })
+  );
+
+  return mappedGrades;
+}
+
+// utils.ts
+
+export const isFutureDate = (dateTimeString: string): boolean => {
+  const currentDate = new Date();
+  const eventDate = new Date(dateTimeString);
+
+  // Compare the dates (ignoring time)
+  currentDate.setHours(0, 0, 0, 0);
+  eventDate.setHours(0, 0, 0, 0);
+
+  return eventDate >= currentDate;
+};
