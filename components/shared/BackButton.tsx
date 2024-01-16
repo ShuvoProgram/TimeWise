efacfc3cc86100112a92898da87670a5e490dd60
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-16 17:36:55 +0600

    Add BackButton component with client-side navigation

---
+++ components/shared/BackButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import { ChevronLeft } from "lucide-react";
+
+function BackButton() {
+  const router = useRouter();
+  return (
+    <Button
+      onClick={() => router.back()}
+      size="lg"
+      className="flex flex-row gap-1 items-center pr-10 w-full md:w-fit opacity-60 hover:opacity-100 transition-all"
+    >
+      <ChevronLeft size={18} /> Back
+    </Button>
+  );
+}
+
+export default BackButton;
